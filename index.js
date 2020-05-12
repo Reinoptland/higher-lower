c2e0dae4f0495f540f6d3198765840985c7e4775
Reinoptland <reinoptland@gmail.com>
2020-05-12 15:00:09 +0200

    Added validations

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// take input from user, argv is an array of strings, we want the 3rd element
 var userInput = parseInt(process.argv[2]);
+// hardcoding 7 as number to guess, stroin in a variable
 var numberToGuess = 7;
 var min = 1;
 var max = 50;
@@ -6,18 +8,27 @@ var max = 50;
 console.log("input:", userInput);
 console.log("number to guess:", numberToGuess);
 console.log("min", min, "max", max);
-
-console.log(userInput < numberToGuess);
+console.log(userInput < numberToGuess); // 1
 // prediction: false
+
+if (userInput < min || userInput > max || isNaN(userInput)) {
+  // 4
+  console.log(
+    `This number is invalid, please guess a number between ${min} and ${max}`
+  );
+
+  process.exit();
+}
 
 if (userInput < numberToGuess) {
   console.log("You should guess a higher number");
-}
-
-if (userInput > numberToGuess) {
+} else if (userInput > numberToGuess) {
   console.log("You should guess a lower number");
+} else if (userInput === numberToGuess) {
+  console.log("CORRECT it was", numberToGuess);
+} else {
+  console.log("Error");
 }
 
-if (userInput === numberToGuess) {
-  console.log("CORRECT it was", numberToGuess);
-}
+// How many boolean expressions are in this code
+// a statement, that resolves to true or false
