7ad0f8400198b2516922bc30f5665956172d8b94
Reinoptland <reinoptland@gmail.com>
2020-05-12 14:36:01 +0200

    Simple working version

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ var max = 50;
 console.log("input:", userInput);
 console.log("number to guess:", numberToGuess);
 console.log("min", min, "max", max);
+
+console.log(userInput < numberToGuess);
+// prediction: false
+
+if (userInput < numberToGuess) {
+  console.log("You should guess a higher number");
+}
+
+if (userInput > numberToGuess) {
+  console.log("You should guess a lower number");
+}
+
+if (userInput === numberToGuess) {
+  console.log("CORRECT it was", numberToGuess);
+}
